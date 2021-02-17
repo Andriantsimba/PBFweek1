095e69ff0c2591d45aca8deee1649507d8ab587e
Andriantsimba <mami28niaina@gmail.com>
2021-02-17 10:50:36 +0700

    ReactJS Week2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello =()=>{
+  return<p>hellow</p>
+}
+
+// ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Hello/>, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
