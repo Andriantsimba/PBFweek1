ef143c77ad0b2d8a3523d36cf06a002c51e86b8a
Andriantsimba <mami28niaina@gmail.com>
2021-04-14 08:34:42 +0700

    week 1

---
+++ src/Components/HelloComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HelloComponent.css';
+
+const HelloComponent = () => {
+    return <p className='text-p'>This is an arrow function wich is in the folder</p>
+}
+
+export default HelloComponent;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
 
-const Hello =()=>{
-  return<p>hellow</p>
-}
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Test from './Components/Test'
+import HelloComponent from './Components/HelloComponent';
+
+import CreateToDo from './Containers/createTodo'
+import Table from './Containers/Table'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import MainReducer from './reducers/MainReducer'
+
+
+// const HelloComponent = () => 
+// {
+//   return HelloComponent
+// }
+
+// class StateFullComponent extends React.Component{
+//   render(){
+//     return <p>StateFullComponent</p>
+//   }
+// }
+
+//   ReactDOM.render
+//   (<HelloComponent />, 
+//     document.getElementById('root'));
+
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => 
+f)(createStore)(MainReducer)
+ReactDOM.render(
+    <Provider store={store}>
+         <CreateToDo />
+         <Table />
+    </Provider>
+, document.getElementById('root'));
+
+
 
 // ReactDOM.render(<App/>, document.getElementById('root'));
-ReactDOM.render(<Hello/>, document.getElementById('root'));
+// ReactDOM.render(<Hello/>, document.getElementById('root'));
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -18,7 +50,4 @@ ReactDOM.render(<Hello/>, document.getElementById('root'));
 //   document.getElementById('root')
 // );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
